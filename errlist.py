0eacfdf50fe03c526643de181575b7d2819d2c40
Rooparaj597 <rooparaj597@gmail.com>
2025-08-10 13:14:17 +0530

    Delete errlist.py

--- errlist.py
+++
@@ -1,8 +0,0 @@
-fruits = ["Cherry", "Apple", "Pear"]
-veg = ["Cucumber", "Kale", "Spinnach"]
-
-#to combine to list in one list use list1,list2
-
-fruits_and_veg = [fruits+veg]
-
-print(fruits_and_veg)
